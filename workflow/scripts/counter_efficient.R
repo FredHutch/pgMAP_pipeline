7ce1b7522b97b08c8a759722666fd8a7c97726fc
Daniel J. Groso <95438884+danieljgroso@users.noreply.github.com>
2023-05-17 15:36:15 -0700

    Update counter_efficient.R

--- workflow/scripts/counter_efficient.R
+++ workflow/scripts/counter_efficient.R
@@ -1,20 +1,14 @@
-## TO DO
-## - make reference file an input as well
-## - make sure file name format can change easily
-## - make sure file name format matches snakemake pipeline
-
-
 ############################################################
 ## Load packages                                          ##
 ############################################################
 
-## rhino module: R-3_6_2-foss-2019b-fh1
+## rhino module: R-3.6.2-foss-2019b-fh1
 ## if running code in Rstudio on gizmo via launch_rstudio_server:
-##   run this line before Rsamtools will load: _libPaths(c("/app/software/R/3_6_2-foss-2018b-fh1", _libPaths()))
+##   run this line before Rsamtools will load: .libPaths(c("/app/software/R/3.6.2-foss-2018b-fh1", .libPaths()))
 
 ## packages to load: Rsamtools, tidyverse
 import_library = function(lib_name){
-  suppressWarnings(suppressMessages(require(lib_name, character_only=TRUE)))
+  suppressWarnings(suppressMessages(require(lib_name, character.only=TRUE)))
 }
 import_library("Rsamtools") # new version = 1.34.1 # now 2.2.3
 import_library("tidyverse") # new version = 1.2.1 # now 1.3.0
@@ -34,37 +28,37 @@ args <- commandArgs(trailingOnly=TRUE)
 ## make sure that a file name has been supplied as an argument
 if(length(args) < 4){
   stop("Please supply all required arguments!",
-       call_=FALSE)
+       call.=FALSE)
 }
 
 ## assign args[1] to filename variable:
-files_1 <- args[1]
-message(paste0("gRNA1 BAM file read in as:", files_1))
+files.1 <- args[1]
+message(paste0("gRNA1 BAM file read in as:", files.1))
 
-## replace gRNA_1 with gRNA_2 in files_2 variable
-files_2 <- gsub ("R1", "R2", files_1)
+## replace gRNA_1 with gRNA_2 in files.2 variable
+files.2 <- gsub ("R1", "R2", files.1)
 
-if (!all (file_exists (files_2)))
-  stop ("Failed to find properly matched BAM files for reads 1 and 2_")
+if (!all (file.exists (files.2)))
+  stop ("Failed to find properly matched BAM files for reads 1 and 2.")
 
-## assign args[2] to n_chunks variable
-n_chunks <- as_numeric(args[2])
+## assign args[2] to n.chunks variable
+n.chunks <- as.numeric(args[2])
 
-## assign args[3] to ref_file variable, which will be used to build d.counts
-ref_file <- args[3]
-stopifnot (file_exists (ref_file))
+## assign args[3] to ref.file variable, which will be used to build d.counts
+ref.file <- args[3]
+stopifnot (file.exists (ref.file))
 
 ## Parse annotation file and initialize d.counts, which holds raw counts of reads
-## supporting each pgRNA_
-d.counts <- read_tsv (ref_file, col_names=TRUE, col_types=cols())
+## supporting each pgRNA.
+d.counts <- read_tsv (ref.file, col_names=TRUE, col_types=cols())
 
-## assign args[4] to counts_dir variable
-counts_dir <- args[4]
+## assign args[4] to counts.dir variable
+counts.dir <- args[4]
 
-## if this ends in a "/", remove it___not sure if this is necessary though
-# message(paste0("counts_dir = ", counts_dir))
-if(str_sub(counts_dir, -1) == "/"){
-  str_sub(counts_dir, -1, -1) <- ""
+## if this ends in a "/", remove it...not sure if this is necessary though
+# message(paste0("counts.dir = ", counts.dir))
+if(str_sub(counts.dir, -1) == "/"){
+  str_sub(counts.dir, -1, -1) <- ""
 }
 
 ############################################################
@@ -73,21 +67,21 @@ if(str_sub(counts_dir, -1) == "/"){
 
 ## Goals:
 ## - Compute read counts supporting each pgRNA and store in columns named
-##   counts_sample in d.counts
+##   counts_sample in d.counts.
 
 
 ## parse sample information from BAM filename
-## 1_ extract input file name from full path
-file_name <- str_split_fixed(files_1, "\\/", n = Inf)
+## 1. extract input file name from full path
+file_name <- str_split_fixed(files.1, "\\/", n = Inf)
 file_name <- file_name[length(file_name)]
 
-## 2_ extract sample name from input file
+## 2. extract sample name from input file
 sample_tmp <- str_split_fixed(file_name, "_aligned", n = Inf)[1]
 sample <- str_split_fixed(sample_tmp, "trimmed_", n = Inf)[2]
 
 
 ## start timer:
-timing <- Sys_time()
+timing <- Sys.time()
 message(paste0("Parsing reads for sample ", sample, "\n"))
 
 ## define parameters for reading in BAM files, then read them in:
@@ -100,13 +94,13 @@ param <- ScanBamParam(
   what = c("qname", "rname")
 )
 
-bam_1 <- scanBam(files_1, param=param)
-bam_2 <- scanBam(files_2, param=param)
+bam.1 <- scanBam(files.1, param=param)
+bam.2 <- scanBam(files.2, param=param)
 
 message("BAM files read in\n")
 
 ## get unique qnames:
-d.qnames <- tibble("qname"=bam_1[[1]]$qname)
+d.qnames <- tibble("qname"=bam.1[[1]]$qname)
 qnames <- unique(d.qnames %>% dplyr::pull("qname"))
 
 message("Qnames extracted\n")
@@ -117,57 +111,57 @@ message("Qnames extracted\n")
 ##   then splits those into separate sub-lists, and then assigns each qname to a sublist (group).
 chunk <- function(x,n) split(x, factor(sort(rank(x)%%n)))
 
-## run chunk() function on n_chunks
-qnames_chunks <- chunk(qnames, n_chunks)
+## run chunk() function on n.chunks
+qnames_chunks <- chunk(qnames, n.chunks)
 
 ## lapply statement that loops through qname chunks
 n <- c(1:length(qnames_chunks))
 results <- lapply(n, function(i){
   sub_qnames <- qnames_chunks[i] %>% unlist()
 
-  sub1 <- subset(DataFrame(bam_1[[1]]),qname %in% sub_qnames)
-  sub2 <- subset(DataFrame(bam_2[[1]]),qname %in% sub_qnames)
+  sub1 <- subset(DataFrame(bam.1[[1]]),qname %in% sub_qnames)
+  sub2 <- subset(DataFrame(bam.2[[1]]),qname %in% sub_qnames)
 
   # convert DataFrame to tibble:
-  d.bam_1_sub <- sub1 %>% as_tibble() %>% mutate_at("rname", as_character)
-  d.bam_2_sub <- sub2 %>% as_tibble() %>% mutate_at("rname", as_character)
+  d.bam.1.sub <- sub1 %>% as_tibble() %>% mutate_at("rname", as.character)
+  d.bam.2.sub <- sub2 %>% as_tibble() %>% mutate_at("rname", as.character)
 
   ## perform inner join between alignments of read 1 and 2 in order to obtain
   ##    all possible pairings implied by each read alignment
-  d.bam_sub <- inner_join(d.bam_1_sub %>% select (qname, "rname_1" = rname),
-                         d.bam_2_sub %>% select (qname, "rname_2" = rname),
+  d.bam.sub <- inner_join(d.bam.1.sub %>% select (qname, "rname.1" = rname),
+                         d.bam.2.sub %>% select (qname, "rname.2" = rname),
                          by = "qname")
 
   ## check whether each pairing is correct
-  d.bam_sub <- d.bam_sub %>% mutate("paired" = rname_1 == rname_2)
+  d.bam.sub <- d.bam.sub %>% mutate("paired" = rname.1 == rname.2)
 
   ## if a given set of reads have one or more correct pairings, then keep the
-  ##   correct pairings and discard all incorrect pairings for those reads
-  qname2anypaired <- sapply (split (d.bam_sub$paired, f = d.bam_sub$qname), any)
-  d.bam_sub <- d.bam_sub %>% left_join (tibble ("qname" = names (qname2anypaired),
+  ##   correct pairings and discard all incorrect pairings for those reads.
+  qname2anypaired <- sapply (split (d.bam.sub$paired, f = d.bam.sub$qname), any)
+  d.bam.sub <- d.bam.sub %>% left_join (tibble ("qname" = names (qname2anypaired),
                                                "any_paired" = qname2anypaired),
                                        by = "qname") %>%
     filter (paired | !any_paired) %>%
     select (-any_paired)
 
   ## compute weights for each set of reads
-  qname2n <- table (d.bam_sub$qname)
-  d.bam_sub <- d.bam_sub %>% left_join (tibble ("qname" = names (qname2n),
-                                                "n" = as_integer (qname2n)),
+  qname2n <- table (d.bam.sub$qname)
+  d.bam.sub <- d.bam.sub %>% left_join (tibble ("qname" = names (qname2n),
+                                                "n" = as.integer (qname2n)),
                                        by = "qname") %>%
     mutate ("weight" = 1 / n) %>%
     select (-n)
 
   ## store statistics on the numbers of correctly paired reads in a tibble
-  n_paired <- d.bam_sub %>% filter (paired) %>% summarize (sum (weight)) %>% collect() %>% _[[1]]
-  n <- d.bam_sub %>% summarize (sum (weight)) %>% collect() %>% _[[1]]
-  d.paired_sub <- tibble("n_correctly_paired"=n_paired, "n_total"=n)
+  n.paired <- d.bam.sub %>% filter (paired) %>% summarize (sum (weight)) %>% collect() %>% .[[1]]
+  n <- d.bam.sub %>% summarize (sum (weight)) %>% collect() %>% .[[1]]
+  d.paired.sub <- tibble("n.correctly.paired"=n.paired, "n.total"=n)
 
   ## compute counts for correctly paired reads
-  d.bam_sub <- d.bam_sub %>% filter (paired)
+  d.bam.sub <- d.bam.sub %>% filter (paired)
 
   ## return a list of the d.bam and d.paired for this subset of the BAM file
-  return(list(bam=d.bam_sub, paired=d.paired_sub))
+  return(list(bam=d.bam.sub, paired=d.paired.sub))
 
 })
 
@@ -184,72 +178,37 @@ d.paired <- lapply(n, function(i){
 }) %>% bind_rows()
 
 ## calculate pairing statistics
-d.paired_all <- d.paired %>% summarize(n_paired=sum(n_correctly_paired), n_total=sum(n_total))
-d.paired_all <- d.paired_all %>% mutate("pct_paired"=round(100*n_paired/n_total,3))
+d.paired.all <- d.paired %>% summarize(n.paired=sum(n.correctly.paired), n.total=sum(n.total))
+d.paired.all <- d.paired.all %>% mutate("pct.paired"=round(100*n.paired/n.total,3))
 
-message (paste0 ("   ", d.paired_all$n_paired, " / ", d.paired_all$n_total,
-                 " (", d.paired_all$pct_paired, "%) correctly paired reads\n"))
+message (paste0 ("   ", d.paired.all$n.paired, " / ", d.paired.all$n.total,
+                 " (", d.paired.all$pct.paired, "%) correctly paired reads\n"))
 
 ## check that all rnames are correctly paired
-stopifnot (all (d.bam$rname_1 == d.bam$rname_2))
+stopifnot (all (d.bam$rname.1 == d.bam$rname.2))
 
 ## calculate guide counts based on weights
 d.bam <- d.bam %>%
-  select ("id" = rname_1, weight) %>%
+  select ("id" = rname.1, weight) %>%
   group_by (id) %>% summarize ("counts" = sum (weight)) %>% ungroup()
 
 ## store counts
-d.counts <- d.counts %>% left_join (d_bam, by = "id")
+d.counts <- d.counts %>% left_join (d.bam, by = "id")
 rm (d.bam)
 
 ## some pgRNAs may have no counts; replace those NAs with 0
-d.counts$counts[is_na (d.counts$counts)] = 0
+d.counts$counts[is.na (d.counts$counts)] = 0
 
 ## rename counts column
-new_counts <- paste0("counts_", sample)
-d.counts <- d.counts %>% dplyr::rename(!!new_counts := counts)
+new.counts <- paste0("counts_", sample)
+d.counts <- d.counts %>% dplyr::rename(!!new.counts := counts)
 
 message("Counts file made\n")
 
 ## write per-sample output to tab-delimited counts file
-counts_filename <- paste0("counts_", sample, "_txt")
-counts_filepath <- file_path(counts_dir, counts_filename)
-write_tsv(d.counts, counts_filepath)
-message(paste0("Counts file written to: ", counts_filepath))
+counts.filename <- paste0("counts_", sample, ".txt")
+counts.filepath <- file.path(counts.dir, counts.filename)
+write_tsv(d.counts, counts.filepath)
+message(paste0("Counts file written to: ", counts.filepath))
 
-message (paste0 ("Done (", signif (as_numeric (difftime (Sys_time(), timing, units = "mins")), 2), "m elapsed)_\n"))
-
-
-############################################################
-## Notes on potentially making the script faster          ##
-############################################################
-
-# optional: save BAM (either full or subsetted) as RDS & read back in
-# saveRDS(test1, file_path(basedir, "bowtie1_aligned/200722_HeLa_screen/bam_sorted/test_case","test1_bam"))
-# saveRDS(test2, file_path(basedir, "bowtie1_aligned/200722_HeLa_screen/bam_sorted/test_case","test2_bam"))
-# d.bam_1 = readRDS("test1_bam") %>% as_tibble()
-# d.bam_2 = readRDS("test2_bam") %>% as_tibble()
-
-### Within the lapply:
-## for converting BAMs to tibbles we could use an all-purpose conversion method as follows:
-##   d.bam_1 = do_call ("data_frame", bam_1) %>% as_tibble()
-##   d.bam_2 = do_call ("data_frame", bam_2) %>% as_tibble()
-## however, it's much faster to manually construct the tibbles
-##   d.bam_1_sub = tibble("qname" = d.bam_1_sub[[1]]$qname, "rname" = d.bam_1_sub[[1]]$rname)
-##   d.bam_1_sub <- d.bam_1_sub %>% mutate_at("rname", as_character)
-##   d.bam_1 = tibble ("qname" = bam_1[[1]]$qname,
-##                     "rname" = factor2character (bam_1[[1]]$rname))
-
-## For filtering out incorrectly paired reads:
-## The simplest way to achieve this is by grouping over reads as follows:
-##   d.bam = d.bam %>% group_by (qname) %>%
-##     mutate ("any_paired" = any (paired)) %>% ungroup() %>%
-##     filter (paired | !any_paired) %>%
-##     select (-any_paired)
-## However, grouping over millions of reads is very slow. Therefore, do it manually.
-
-## compute weights for each set of reads
-## As above, the straightforward method is slow:
-##   d.bam = d.bam %>% group_by (qname) %>%
-##     mutate ("weight" = 1 / n()) %>% ungroup()
-## Therefore, do it manually for speed
+message (paste0 ("Done (", signif (as.numeric (difftime (Sys.time(), timing, units = "mins")), 2), "m elapsed).\n"))
